a63519263d0b87fe754b0596061b975504b5a4b8
Tinodaishe Tembo <synthmancer@gmail.com>
2024-03-04 10:38:11 +0200

    add mock data

---
+++ mock/data.js
@@ -0,0 +1,58 @@
+export const team = [
+	{
+		name: "Musa",
+		picture: "",
+		linkedin: "",
+		github: "",
+		site: "",
+	},
+	{
+		name: "Prince",
+		picture: "",
+		linkedin: "",
+		github: "",
+		site: "",
+	},
+	{
+		name: "Tembo",
+		picture: "",
+		linkedin: "",
+		github: "",
+		site: "",
+	},
+	{
+		name: "Erlson",
+		picture: "",
+		linkedin: "",
+		github: "",
+		site: "",
+	},
+];
+
+export const papers = [
+	{
+		name: "Generative Agents: A Simulacra Of Human Behavior",
+		date: "",
+		source: "",
+		link: "",
+	},
+	{
+		name: "Attention is All you need",
+		date: "",
+		source: "",
+		link: "",
+	},
+];
+
+export const posts = [
+	{
+		title: "",
+		date: "",
+		summary: "",
+	},
+	{
+		title: "",
+		date: "",
+		summary: "",
+	},
+];
