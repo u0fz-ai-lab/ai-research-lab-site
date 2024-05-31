6c8928a6e1c4ebad9c613aa223c696e50f125479
Tinotenda Makamba <109346990+tcrowdt@users.noreply.github.com>
2024-05-31 16:11:21 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,64 @@
 import React from "react";
+import "./footer.css";
 
 const Footer = () => {
-	return (
-		<div className="flex justify-between items-center">
-			<h1>&copy; 2024, AiLab</h1>
-			<p className="font-mono">e/acc</p>
-		</div>
-	);
+  return (
+    <div className="footer-container">
+      <div className="flex justify-between items-center">
+        <ul className="footer-list-left">
+          <li>
+            <a href="./app/about/page.jsx">About</a>
+          </li>
+          <li>
+            <a href="./team/page.tsx">Team</a>
+          </li>
+          <li>
+            <a href="./research/page.tsx">Research</a>
+          </li>
+        </ul>
+        <ul className="footer-list-right">
+          <li>
+            <a
+              href="https://github.com/your-username"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.facebook.com/your-username"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Facebook
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.twitter.com/your-username"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.instagram.com/your-username"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Instagram
+            </a>
+          </li>
+        </ul>
+      </div>
+      <br />
+      <h1>&copy; 2024 AiLab. All Rights Reserved</h1>
+    </div>
+  );
 };
 
 export default Footer;
